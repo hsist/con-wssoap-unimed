91d2e986d0a2e67a8b2abb8846e548b6b7346013
danielbonatti <bonattidaniel@hotmail.com>
2023-08-31 09:42:43 -0300

    ajuste p/ contemplar o cancelamento

--- index.php
+++ index.php
@@ -7,7 +7,7 @@ $method = $argv[3];
 $options = array(
     'location' => $endpoint,
     'uri' => 'http://www.ans.gov.br/padroes/tiss/schemas', 
-    'trace' => 1, // Ativar rastreamento
+    'trace' => 1,      // Ativar rastreamento
     'exceptions' => 1, // Ativar exceções
 );
 
@@ -15,10 +15,21 @@ $options = array(
 $client = new SoapClient(null, $options);
 
 try {
+    $noh = '';
+    if ($method == 'tissSolicitacaoProcedimento_Operation') {
+        $noh = 'autorizacaoProcedimentoWS';
+    } elseif ($method == 'tissCancelaGuia_Operation') {
+        $noh = 'reciboCancelaGuiaWS';
+    } else {
+        $arr = array('status' => false, 'method' => 'metodo nao previsto');
+        echo json_encode($arr);
+        return false;
+    }
+
     // Invoca o método da API SOAP com o conteúdo XML e método específico
     $response = $client->__doRequest($xmlContent, $endpoint, $method, SOAP_1_1, 0);
     $xmlObject = simplexml_load_string($response);
-    $result = $xmlObject->children('soap', true)->children('ans', true)->autorizacaoProcedimentoWS;
+    $result = $xmlObject->children('soap', true)->children('ans', true)->$noh;
     $jsonString = json_encode($result, JSON_PRETTY_PRINT);
     echo $jsonString;
 
