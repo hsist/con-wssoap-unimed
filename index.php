6bc4d6349efa47819fd3170a83ff5607636c44f8
danielbonatti <bonattidaniel@hotmail.com>
2024-04-02 16:02:03 -0300

    inclusao de mais um parametro

--- index.php
+++ index.php
@@ -2,11 +2,13 @@
 $endpoint = $argv[1];
 $xmlContent = $argv[2];
 $method = $argv[3];
+$verSoap = $argv[4] ?? '1';
 
-/*$filePath = __DIR__ . '/meuarquivo.xml';
+/*$endpoint = 'http://s20atd-hml.avare.sgusuite.com.br/WSTiss3/http/v4_01_00/tissSolicitacaoProcedimento';
+$filePath = __DIR__ . '/meuarquivo.xml';
 $xmlContent = file_get_contents($filePath);
-$endpoint = $argv[1];
-$method = $argv[2];*/
+$method = 'tissSolicitacaoProcedimento_Operation';
+$verSoap = $argv[1] ?? '1';*/
 
 // Opções do cliente SOAP
 $options = array(
@@ -32,7 +34,11 @@ try {
     }
 
     // Invoca o método da API SOAP com o conteúdo XML e método específico
-    $response = $client->__doRequest($xmlContent, $endpoint, $method, SOAP_1_1, 0);
+    if ($verSoap == '1') {
+        $response = $client->__doRequest($xmlContent, $endpoint, $method, SOAP_1_1, 0);
+    } else {
+        $response = $client->__doRequest($xmlContent, $endpoint, $method, SOAP_1_2);
+    }
 
     // Verificar se a resposta contém um <soap:Fault>
     if (strpos($response, '<soap:Fault>') !== false) {
@@ -45,13 +51,14 @@ try {
         return false;
     }
 
-    $xmlObject = simplexml_load_string($response);
+    //$xmlObject = simplexml_load_string($response);
+    $xmlObject = simplexml_load_string($response, 'SimpleXMLElement', 0, 'http://schemas.xmlsoap.org/soap/envelope/');
     if (!$xmlObject) {
         echo json_encode(['status' => false, 'error' => 'Falha ao processar a resposta XML.'], JSON_PRETTY_PRINT);
         return false;
     }
 
-    //$result = $xmlObject->xpath('//soap:ans');
+    //$result = $xmlObject->xpath('//ans:'.$noh);
     $result = $xmlObject->children('soap', true)->Body->children('ans', true)->$noh;
     if (empty($result)) {
         echo json_encode(['status' => false, 'error' => 'Resposta inesperada.'], JSON_PRETTY_PRINT);
